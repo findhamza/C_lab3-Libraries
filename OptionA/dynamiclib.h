e105d017f3cd835ba108bfd164a1586a4cef202c
findhamza <iamhamza@live.com>
2019-04-02 10:23:14 -0500

    Option A makefile finally funtional

--- OptionA/dynamiclib.h
+++ OptionA/dynamiclib.h
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 
-float factorial(float x);
 void sphere(float radius, float *surface, float *volume);
 float volCylinder(float radius, float height);
 float sumFloats(float x[], int numFloats);
